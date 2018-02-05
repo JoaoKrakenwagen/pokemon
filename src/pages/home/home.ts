f4ca56583d8887c9c813df78e97abbabe43b5a59
Joao_Pedro <joaopedrodepaula@ejcm.com.br>
2018-02-05 16:33:15 -0200

    + exercicio aula ionic

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { PokemonProvider } from '../../providers/pokemon/pokemon';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  pokemon = [];
+
+  nextId: number;
+
+  constructor(public navCtrl: NavController, public pokemonProvider: PokemonProvider) {
+    this.nextId = 1;
+  }
+
+  getPokemon () {
+    this.pokemonProvider.getPokemon(this.nextId).subscribe(
+      (res)=>{
+        this.pokemon.push(res);
+        console.log(this.pokemon);
+        this.nextId++;
+      },
+    )
+  }
+
+}''
